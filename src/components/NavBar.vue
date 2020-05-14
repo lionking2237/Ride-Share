66372688d3f787c51d30a79e4a0b3f3e96f95cc0
leviwhite12 <64931209+leviwhite12@users.noreply.github.com>
2020-05-13 22:06:43 -0400

    Add files via upload

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -9,6 +9,8 @@
     <v-spacer/>
 
     <v-btn text v-bind:to="{ name: 'about-us' }">About Us</v-btn>
+    <v-btn text v-bind:to="{name:'applyrideshare'}">Apply for Ride Share</v-btn>
+    <v-btn text v-bind:to="{name:'authorize'}">Authorize Driver</v-btn>
     <v-btn text v-bind:to="{name:'rides'}">Rides</v-btn>
     <v-btn text v-bind:to="{ name: 'locations' }">Locations</v-btn>
     <v-btn text v-bind:to="{name:'vehicles'}"> Create Vehicles</v-btn>
