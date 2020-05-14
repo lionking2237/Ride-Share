097b522ef7dcba5e046bdfc2999dc6b3ca5c185d
leviwhite12 <64931209+leviwhite12@users.noreply.github.com>
2020-05-13 22:07:09 -0400

    Add files via upload

---
+++ src/pages/ApplyRideShare.vue
@@ -0,0 +1,57 @@
+<template>
+   <v-container>
+       <div><h4 class="display-1">Apply for Passenger or Driver</h4></div>
+       <div>
+           <v-form v-model="valid">
+               <v-text-field
+               v-bind:rules="rules.required"
+               label="Passenger Name">
+               </v-text-field>
+               <v-btn v-bind:disabled="!valid" v-on:click="handleSubmit"
+            >Add Passenger
+            </v-btn>
+               <v-text-field
+               v-bind:rules="rules.required"
+               label="Driver Name">
+               </v-text-field>
+               <v-btn v-bind:disabled="!valid" v-on:click="handleSubmit"
+            >Add Driver
+            </v-btn>
+               </v-form>
+       </div>
+   </v-container>
+</template>
+
+<script>
+    export default {
+        name: "ApplyRideShare",
+        data: function() {
+            return {
+                headers: [
+                    {text: "Date", value: "date"},
+                    {text: "Time", value: "time"},
+                    {text: "Distance", value: "distance"},
+                    {text: "Fuel Price", value: "fuelPrice"},
+                    {text: "Fee", value: "fee"},
+                ],
+                rides: [],
+                rules:{
+                    licenceNum:[(val) => val.length > 0 && val.length < 16],
+                    required: [(val) => val.length > 0|| (val)>0 || "Required"],
+                },
+            }
+        },
+        handleSubmit: function() {
+            return "test";
+        },
+        mounted: function () {
+            this.$axios.get("/rides").then(response => {
+                console.log("RESPONSE", response);
+                this.rides = response.data;
+            })
+        }
+    }
+</script>
+
+<style scoped>
+</style>
---
+++ src/pages/Authorize.vue
@@ -0,0 +1,54 @@
+<template>
+   <v-container>
+       <div><h4 class="display-1">Driver Authorization</h4></div>
+       <div>
+           <v-form v-model="valid">
+               <v-overflow-btn
+               v-bind:rules="rules.required"
+               label="Driver">
+               </v-overflow-btn>
+               <v-overflow-btn
+               v-bind:rules="rules.required"
+               label="Vehicle">
+               </v-overflow-btn>
+               <v-btn v-bind:disabled="!valid" v-on:click="handleSubmit"
+            >Add Vehicle
+            </v-btn>
+               </v-form>
+       </div>
+   </v-container>
+</template>
+
+<script>
+    export default {
+        name: "Authorize",
+        data: function() {
+            return {
+                headers: [
+                    {text: "Date", value: "date"},
+                    {text: "Time", value: "time"},
+                    {text: "Distance", value: "distance"},
+                    {text: "Fuel Price", value: "fuelPrice"},
+                    {text: "Fee", value: "fee"},
+                ],
+                rides: [],
+                rules:{
+                    licenceNum:[(val) => val.length > 0 && val.length < 16],
+                    required: [(val) => val.length > 0|| (val)>0 || "Required"],
+                },
+            }
+        },
+        handleSubmit: function() {
+            return "test";
+        },
+        mounted: function () {
+            this.$axios.get("/rides").then(response => {
+                console.log("RESPONSE", response);
+                this.rides = response.data;
+            })
+        }
+    }
+</script>
+
+<style scoped>
+</style>
