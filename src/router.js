b5cee8a576e205e19fec19a01f6039beb32e70ac
leviwhite12 <64931209+leviwhite12@users.noreply.github.com>
2020-05-19 13:24:54 -0400

    Add files via upload

--- src/router.js
+++ src/router.js
@@ -2,7 +2,6 @@ import Vue from "vue";
 import Router from "vue-router";
 
 import Home from "./pages/Home.vue";
-import About from "./pages/About.vue";
 import Locations from "./pages/Locations";
 import Vehicles from "./pages/Vehicles";
 import Rides from "./pages/Rides";
@@ -20,7 +19,6 @@ export default new Router({
   base: process.env.BASE_URL,
   routes: [
     { name: "home-page", path: "/", component: Home },
-    { name: "about-us", path: "/about-us", component: About },
     { name: "locations", path: "/locations", component: Locations },
     { name: "vehicles", path: "/vehicles", component: Vehicles },
     { name: "rides", path: "/rides", component: Rides },
