8179f98b3e6d708910cde4e9764ff378a655b9ce
leviwhite12 <64931209+leviwhite12@users.noreply.github.com>
2020-05-18 21:51:15 -0400

    Add files via upload

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -9,14 +9,14 @@
     <v-spacer/>
 
     <v-btn text v-bind:to="{ name: 'about-us' }">About Us</v-btn>
-    <v-btn text v-bind:to="{name:'applyrideshare'}">Apply for Ride Share</v-btn>
-    <v-btn text v-bind:to="{name:'authorize'}">Authorize Driver</v-btn>
-    <v-btn text v-bind:to="{name:'rides'}">Rides</v-btn>
     <v-btn text v-bind:to="{ name: 'locations' }">Locations</v-btn>
-    <v-btn text v-bind:to="{name:'vehicles'}"> Create Vehicles</v-btn>
+    <v-btn text v-bind:to="{name:'rides'}">Rides</v-btn>
     <v-btn text v-bind:to="{name:'vehicleslist'}">Vehicles List</v-btn>
-    <v-btn text v-bind:to="{name:'ride-sign-up'}">Ride Sign Up</v-btn>
+    <v-btn text v-bind:to="{name:'vehicles'}"> Create Vehicles</v-btn>
     <v-btn text v-bind:to="{name:'drive-sign-up'}">Driver Sign Up</v-btn>
+    <v-btn text v-bind:to="{name:'authorize'}">Authorize Driver</v-btn>
+    <v-btn text v-bind:to="{name:'ride-sign-up'}">Ride Sign Up</v-btn>
+    <v-btn text v-bind:to="{name:'applyrideshare'}">Apply for Ride Share</v-btn>
   </v-app-bar>
 </template>
 
